2306cbc64227f13f91c5fb79405a5118117e8bf3
Matthew Bryant <snowsignups@gmail.com>
2020-07-03 17:39:51 -0400

    deleted some duplicates left over

--- Color_Test/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("\033[0;31mHello\033[00m \033[01;32mFaizan\033[00m\n");
-}
-
--- Hello_World/main.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <termios.h>
-int main()
-{
-	printf("hello, world\n");
-}	
